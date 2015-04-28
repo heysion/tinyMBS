4e609ec116d70c4e330a5155c4d5ce3710fc2d9b
heysion <heysion@local-laptop.com>
2015-04-28 20:16:23 +0800

    update pushmsg & listmsgs interface  Changes to be committed: 	modified:   src/mbs/mbs_main.py

--- src/mbs/mbs_main.py
+++ src/mbs/mbs_main.py
@@ -14,7 +14,6 @@ class RunMain(tornado.web.Application):
         self.pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
         handlers = [(r"/pushmsg", PushMsgHandler),(r"/listmsgs", GetMsgHandler)]
         tornado.web.Application.__init__(self, handlers, debug=True)
-        pass
 
 class IndexHandler(tornado.web.RequestHandler):
     def post(self):
@@ -36,7 +35,7 @@ class PushMsgHandler(tornado.web.RequestHandler):
                     self.req_json_packet = json.loads(self.request.body)
                     if not self.req_json_packet['c_code']:
                         self.no_error = False
-                    elif not self.req_josn_packet['c_msg']:
+                    elif not self.req_json_packet['c_msg']:
                         self.no_error = False
                 except ValueError, e:
                     self.no_error = False
@@ -49,20 +48,25 @@ class PushMsgHandler(tornado.web.RequestHandler):
                     print e
                 finally:
                     func(self)
+                    print self.req_json_packet
                     print "ending"
         return fn
 
     @__request2json
     @init_db_redis
     def post(self):
+        print self.req_json_packet
         if not self.no_error :
             data = {'retcode':404,'retmsg':'not found'}
             data_json = json.dumps(data)
             print data_json
             self.write(data_json)
         else:
-            self.db.set('remote_ip:%s'%(self.request.remote_ip),"%s"%(self.request.body))
-            self.write(self.request.body)
+            self.req_json_packet['c_id'] = self.db.incr('tinymbs:msgid')
+            print self.req_json_packet['c_id']
+            self.db.lpush('tinymbs:channel:%s'%(self.req_json_packet['c_code']),"%s"%(self.req_json_packet))
+            self.db.set('tinymbs:msgbox:%s'%(self.req_json_packet['c_id']),"%s"%(self.req_json_packet))
+            self.write(self.req_json_packet)
 
     def get(self):
         data = {'retcode':404,'retmsg':'not found'}
@@ -89,22 +93,30 @@ class GetMsgHandler(tornado.web.RequestHandler):
                     func(self)
         return fn
 
-    # @init_db_redis
-    # def post(self):
-    #     self.__request2json()
-    #     self.db.set('remote_ip:%s'%(self.request.remote_ip),"%s"%(self.request.body))
-    #     self.write(self.request.body)
-
     @init_db_redis
     def get(self):
-        data = {'retcode':000,'msg':self.db.get('remote_ip:%s' %(self.request.remote_ip))}
-        data_json = json.dumps(data)
-        self.write(data_json)
-
+        client_code = self.get_argument('client_code', None)
+        if not client_code :
+            data = {'retcode':404,'retmsg':'not found'}
+            data_json = json.dumps(data)
+            print data_json
+            self.write(data_json)
+            return
+        else:
+            ret = self.db.exists('tinymbs:channel:%s'%(client_code))
+            if ret:
+                ret_list = self.db.lrange('tinymbs:channel:%s'%(client_code),0,10)
+                ret_data = json.dumps(ret_list)
+                self.write(ret_data)
+            else:
+                data = {'retcode':404,'retmsg':'not found'}
+                data_json = json.dumps(data)
+                print data_json
+                self.write(data_json)
+                return
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
-#    app = tornado.web.Application(handlers=[(r"/", IndexHandler),(r"/pushmsg",PushMsgHandler)])
     app = RunMain()
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
